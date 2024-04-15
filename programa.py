949b8c7770142983c1cda378201f25ebaf8cd8e8
Daniela Nieto <nietogonzalezdaniela@gmail.com>
2024-04-15 15:15:47 -0500

    Operacion_matrices_numpy v0.0.2

--- programa.py
+++ programa.py
@@ -1,1 +1,21 @@
-#Taller numpy
+#Numpy: biblioteca para realizar cálculos numericos en arreglos n-dimensionales
+#Importar numpy
+import numpy as np
+
+#Crear un ndarray
+#Arreglo de 2 dimensiones creado apartir de una lista
+A=np.array([[1,5],[7,9]])
+B=np.array([[2,0],[1,3]])
+#Producto punto entre ndarray
+C=np.dot(A,B)
+print(C)
+#Solución de un sistema de ecuaciones con numpy
+m_solucion=np.array([5,17])
+m=np.linalg.solve(A,m_solucion)
+print(m)
+
+
+
+
+
+
